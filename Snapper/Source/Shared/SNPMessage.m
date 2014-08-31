b285e4f4dc534a7530feea34c565a8c8565c113e
Paul Schifferer <paul@schifferers.net>
2014-08-31 14:04:46 -0700

    Process entities in messages.

--- Snapper/Source/Shared/SNPMessage.m
+++ Snapper/Source/Shared/SNPMessage.m
@@ -7,6 +7,10 @@
 //
 
 #import "SNPMessage.h"
+
+#import "SNPHashtag.h"
+#import "SNPMention.h"
+#import "SNPLink.h"
 #import "SNPAnnotation.h"
 
 
@@ -143,6 +147,118 @@
                 }
 
                 return [annoDicts copy];
+            }];
+}
+
+
++ (NSValueTransformer*)mentionsJSONTransformer {
+    return [MTLValueTransformer reversibleTransformerWithForwardBlock:
+            ^id(NSArray* mentionsDicts) {
+                NSMutableArray* mentions = [NSMutableArray new];
+
+                for(NSDictionary* mentionDict in mentionsDicts) {
+                    NSError* error = nil;
+                    MTLJSONAdapter* adapter = [[MTLJSONAdapter alloc] initWithJSONDictionary:mentionDict
+                                                                                  modelClass:[SNPMention class]
+                                                                                       error:&error];
+                    if(adapter == nil) {
+                        NSLog(@"Unable to deserialize mention: %@", error);
+                    }
+                    else {
+                        SNPMention* mention = (SNPMention*)[adapter model];
+
+                        [mentions addObject:mention];
+                    }
+                }
+
+                return [mentions copy];
+            }
+                                                         reverseBlock:
+            ^id(NSArray* mentions) {
+                NSMutableArray* mentionsDicts = [NSMutableArray new];
+
+                for(SNPMention* mention in mentions) {
+                    MTLJSONAdapter* adapter = [[MTLJSONAdapter alloc] initWithModel:mention];
+                    NSDictionary* mentionDict = [adapter JSONDictionary];
+
+                    [mentionsDicts addObject:mentionDict];
+                }
+
+                return [mentionsDicts copy];
+            }];
+}
+
++ (NSValueTransformer*)linksJSONTransformer {
+    return [MTLValueTransformer reversibleTransformerWithForwardBlock:
+            ^id(NSArray* linksDicts) {
+                NSMutableArray* links = [NSMutableArray new];
+
+                for(NSDictionary* linkDict in linksDicts) {
+                    NSError* error = nil;
+                    MTLJSONAdapter* adapter = [[MTLJSONAdapter alloc] initWithJSONDictionary:linkDict
+                                                                                  modelClass:[SNPLink class]
+                                                                                       error:&error];
+                    if(adapter == nil) {
+                        NSLog(@"Unable to deserialize mention: %@", error);
+                    }
+                    else {
+                        SNPLink* link = (SNPLink*)[adapter model];
+
+                        [links addObject:link];
+                    }
+                }
+
+                return [links copy];
+            }
+                                                         reverseBlock:
+            ^id(NSArray* links) {
+                NSMutableArray* linksDicts = [NSMutableArray new];
+
+                for(SNPLink* link in links) {
+                    MTLJSONAdapter* adapter = [[MTLJSONAdapter alloc] initWithModel:link];
+                    NSDictionary* linkDict = [adapter JSONDictionary];
+
+                    [linksDicts addObject:linkDict];
+                }
+
+                return [linksDicts copy];
+            }];
+}
+
++ (NSValueTransformer*)hashtagsJSONTransformer {
+    return [MTLValueTransformer reversibleTransformerWithForwardBlock:
+            ^id(NSArray* hashtagsDicts) {
+                NSMutableArray* hashtags = [NSMutableArray new];
+
+                for(NSDictionary* hashtagDict in hashtagsDicts) {
+                    NSError* error = nil;
+                    MTLJSONAdapter* adapter = [[MTLJSONAdapter alloc] initWithJSONDictionary:hashtagDict
+                                                                                  modelClass:[SNPHashtag class]
+                                                                                       error:&error];
+                    if(adapter == nil) {
+                        NSLog(@"Unable to deserialize mention: %@", error);
+                    }
+                    else {
+                        SNPHashtag* hashtag = (SNPHashtag*)[adapter model];
+
+                        [hashtags addObject:hashtag];
+                    }
+                }
+
+                return [hashtags copy];
+            }
+                                                         reverseBlock:
+            ^id(NSArray* hashtags) {
+                NSMutableArray* hashtagsDicts = [NSMutableArray new];
+
+                for(SNPHashtag* hashtag in hashtags) {
+                    MTLJSONAdapter* adapter = [[MTLJSONAdapter alloc] initWithModel:hashtag];
+                    NSDictionary* hashtagDict = [adapter JSONDictionary];
+                    
+                    [hashtagsDicts addObject:hashtagDict];
+                }
+                
+                return [hashtagsDicts copy];
             }];
 }
 
